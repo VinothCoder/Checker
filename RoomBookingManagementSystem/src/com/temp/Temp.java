48ea953d5534cc9efcdb50fc98e5dd48c7684602
vinoth <sarvam.vinoth@gmail.com>
2017-11-08 10:57:14 +0530

    base version

--- RoomBookingManagementSystem/src/com/temp/Temp.java
+++ RoomBookingManagementSystem/src/com/temp/Temp.java
@@ -1,19 +1,10 @@
 package com.temp;
-/**
- * Demo class to check commit messages 
- * @author vinoth
- *
- */
+
 public class Temp {
 
-	//Method added 
-	public void tr()
-	{
-		System.out.println("tr tr ");
-	}
+	
 	
 	private void Psvm() {
-		System.out.println("hiih");
-		Temp jj=new Temp();
+		System.out.println("hiihih");
 	}
 }
